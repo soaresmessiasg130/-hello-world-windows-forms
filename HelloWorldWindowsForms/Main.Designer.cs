c65d4d46217fc42c6b355d35d626f051fff63a04
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2025-06-12 12:31:18 -0300

    Add project files.

---
+++ HelloWorldWindowsForms/Main.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace HelloWorldWindowsForms
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNumber1 = new System.Windows.Forms.TextBox();
+            this.tbNumber2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello, world!!!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click here!!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Calculator - Multiply two numbers";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbNumber1
+            // 
+            this.tbNumber1.Location = new System.Drawing.Point(11, 123);
+            this.tbNumber1.Name = "tbNumber1";
+            this.tbNumber1.Size = new System.Drawing.Size(63, 20);
+            this.tbNumber1.TabIndex = 2;
+            this.tbNumber1.TextChanged += new System.EventHandler(this.tbNumber1_TextChanged);
+            // 
+            // tbNumber2
+            // 
+            this.tbNumber2.Location = new System.Drawing.Point(98, 123);
+            this.tbNumber2.Name = "tbNumber2";
+            this.tbNumber2.Size = new System.Drawing.Size(62, 20);
+            this.tbNumber2.TabIndex = 2;
+            this.tbNumber2.TextChanged += new System.EventHandler(this.tbNumber2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "x";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(190, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Enabled = false;
+            this.btnMultiply.Location = new System.Drawing.Point(166, 121);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(44, 23);
+            this.btnMultiply.TabIndex = 3;
+            this.btnMultiply.Text = "=";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(216, 126);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 0;
+            this.labelResult.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 244);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.tbNumber2);
+            this.Controls.Add(this.tbNumber1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Hello World - Windows Forms";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNumber1;
+        private System.Windows.Forms.TextBox tbNumber2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ HelloWorldWindowsForms/Main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldWindowsForms
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled == true)
+            {
+                button1.Text = "You clicked me!!!";
+
+                return;
+            }
+
+            button1.Enabled = false;
+
+            for (int i = 10; i >= 0; i--)
+            {
+                button1.Enabled = i == 0;
+
+                label1.Text = i == 0 ? "Click now!!!" : "Unlocking the button in " + i;
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbNumber1.Text) && !string.IsNullOrEmpty(tbNumber2.Text))
+            {
+                var number1 = Convert.ToInt32(tbNumber1.Text);
+                var number2 = Convert.ToInt32(tbNumber2.Text);
+                var res = (number1 * number2).ToString();
+
+                labelResult.Text = res.ToString();
+            }
+        }
+
+        private void tbNumber1_TextChanged(object sender, EventArgs e)
+        {
+            unlockMultiplyButton();
+        }
+
+        private void tbNumber2_TextChanged(object sender, EventArgs e)
+        {
+            unlockMultiplyButton();
+        }
+
+        private void unlockMultiplyButton()
+        {
+            btnMultiply.Enabled = !string.IsNullOrEmpty(tbNumber1.Text) && !string.IsNullOrEmpty(tbNumber2.Text);
+        }
+    }
+}
